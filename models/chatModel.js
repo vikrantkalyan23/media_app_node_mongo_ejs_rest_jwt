838dd820920215dc907d1d435f04a13324cbdfbb
vikrantkalyan23 <vikrant.kalyan23@gmail.com>
2024-12-29 12:42:56 +0530

    #update structure

--- models/chatModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ChatSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    message: { type: String, required: true },
-    timestamp: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Chat', ChatSchema);
